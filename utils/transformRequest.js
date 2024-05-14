21ec2fa758a128f478a59fe84e4574f9206ace89
PGRjoystick <30430815+PGRjoystick@users.noreply.github.com>
2024-05-14 22:01:20 +0700

    - use the default api version based on dotenv file

--- utils/transformRequest.js
+++ utils/transformRequest.js
@@ -1,6 +1,6 @@
 module.exports = (originalRequest) => {
   return {
-    version: originalRequest.version || 14,
+    version: originalRequest.version || process.env.DEFAULT_VERSION,
     config: {
       "OUTPUT_STREAM": {
         "provider_id": "openai",
